c3a143140980b37d34ef1cc5aa7b45440e508f65
Chien Tran <70304839+chientrm@users.noreply.github.com>
2022-08-30 10:26:35 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 docker build -t dev .
-docker run -p 52022:22 dev
+docker run\
+  -p 52022:22\
+  -v ~/.ssh/authorized_keys:/root/.ssh/authorized_keys
+  -v ${PWD}:/workspace
+  -w /workspace
+  dev
